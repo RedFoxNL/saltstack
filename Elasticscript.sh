76dcdbd44e12f0568b2f1fc016652188e947e494
Thom Boer <thom.boer@gmail.com>
2017-06-27 12:34:13 +0200

    new updates

--- Elasticscript.sh
+++ Elasticscript.sh
@@ -1,43 +1,44 @@
-#!bin/bash
+#!/bin/bash
 #Het updaten en upgraden van het systeem
-apt update && upgrade -y
+echo "127.0.0.1 testminion" | sudo tee /etc/hosts
+sudo apt update && upgrade -y
 
 #Het installeren van Elasticsearch
-apt install elasticsearch -y
+sudo apt install elasticsearch -y
 
 #Het instellen van het IP van de Hoofd Syslog server
-sed -i '203s/.*/network.bind_host: 10.8.0.28/' /etc/elasticsearch/elasticsearch.yml
-service elasticsearch restart
+sudo sed -i '203s/.*/network.bind_host: 10.8.0.28/' /etc/elasticsearch/elasticsearch.yml
+sudo service elasticsearch restart
 
 #Het uncommenten van de UDP en TCP modules
-sed -i '18s/.*/module(load="imudp")/' /etc/rsyslog.conf
-sed -i '19s/.*/input(type="imudp" port="514")/' /etc/rsyslog.conf
-sed -i '22s/.*/module(load="imtcp")/' /etc/rsyslog.conf
-sed -i '23s/.*/input(type="imtcp" port="514")/' /etc/rsyslog.conf
-service rsyslog restart
+sudo sed -i '18s/.*/module(load="imudp")/' /etc/rsyslog.conf
+sudo sed -i '19s/.*/input(type="imudp" port="514")/' /etc/rsyslog.conf
+sudo sed -i '22s/.*/module(load="imtcp")/' /etc/rsyslog.conf
+sudo sed -i '23s/.*/input(type="imtcp" port="514")/' /etc/rsyslog.conf
+sudo service rsyslog restart
 
 #Het toevoegen van het IP van de server waarmee hij moet connecten
-sed "15i @10.8.0.28" /etc/rsyslog.d/50-default.conf
-service rsyslog restart
+sudo sed -i '15s/.*/@10.8.0.28/' /etc/rsyslog.d/50-default.conf
+sudo service rsyslog restart
 
 #Formatting the Log Data to JSON
-wget -O 01-json-template.conf https://github.com/RedFoxNL/saltstack/blob/master/01-json-template.conf
-cp 01-json-template.conf /etc/rsyslog.d/01-json-template.conf
+sudo wget -O 01-json-template.conf https://github.com/RedFoxNL/saltstack/blob/master/01-json-template.conf
+sudo cp 01-json-template.conf /etc/rsyslog.d/01-json-template.conf
 
 #Het toevoegen van het IP adress van de Logstash server
-sed -i '4s/.*/*.*                         @10.8.0.28:10514;json-template' /etc/rsyslog.d/60-output.conf
+sudo sed -i '4s/.*/*.* @10.8.0.28:10514;json-template/' /etc/rsyslog.d/60-output.conf
 
 #Het installeren van logstash
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list
-apt update
-apt install logstash -y
+sudo wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install logstash -y
 
 #Het ophalen van de logstash.conf
-wget -O logstash.conf https://github.com/RedFoxNL/saltstack/blob/master/logstash.conf
-cp logstash.conf /etc/logstash/conf.d/logstash.conf
-service logstash configtest
-service logstash start
-service rsyslog restart
+sudo wget -O logstash.conf https://github.com/RedFoxNL/saltstack/blob/master/logstash.conf
+sudo cp logstash.conf /etc/logstash/conf.d/logstash.conf
+sudo service logstash configtest
+sudo service logstash start
+sudo service rsyslog restart
  
 
