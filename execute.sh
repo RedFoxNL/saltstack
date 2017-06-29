9110fe4320a596f3a9320b263ad5c08117ded55a
RedFoxNL <thom.boer@gmail.com>
2017-06-29 18:18:06 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -39,43 +39,16 @@ sudo docker run -e WORDPRESS_DB_PASSWORD=Cisco123 --name wordpress --link wordpr
 #Het forwarden van het IP adres
 sudo iptables -P FORWARD ACCEPT
 
-#Het installeren van Elasticsearch
-sudo apt install elasticsearch -y
-
-#Het instellen van het IP van de Hoofd Syslog server
-sudo sed -i '203s/.*/network.bind_host: 10.8.0.28/' /etc/elasticsearch/elasticsearch.yml
-sudo service elasticsearch restart
-
 #Het uncommenten van de UDP en TCP modules
 sudo sed -i '18s/.*/module(load="imudp")/' /etc/rsyslog.conf
 sudo sed -i '19s/.*/input(type="imudp" port="514")/' /etc/rsyslog.conf
 sudo sed -i '22s/.*/module(load="imtcp")/' /etc/rsyslog.conf
 sudo sed -i '23s/.*/input(type="imtcp" port="514")/' /etc/rsyslog.conf
-sudo service rsyslog restart
 
 #Het toevoegen van het IP van de server waarmee hij moet connecten
 sudo sed -i '5s/.*/ *.*\		\@10.8.0.28:514/' /etc/rsyslog.d/50-default.conf
 sudo service rsyslog restart
 
-#Formatting the Log Data to JSON
-sudo git clone https://github.com/RedFoxNL/saltstack.git
-sudo cp saltstack/config/01-json-template.conf /etc/rsyslog.d/01-json-template.conf
-
-#Het installeren van logstash
-sudo wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt install logstash -y
-
-#Het ophalen van de logstash.conf
-sudo cp saltstack/config/logstash.conf /etc/logstash/conf.d/logstash.conf
-sudo sed -i '26s/.*/hosts => [ "'$IP':9200" ]/' /etc/logstash/conf.d/logstash.conf
-sudo service logstash configtest
-sudo service logstash start
-sudo service rsyslog restart
-
-#Het starten van ElasticSearch
-sudo /usr/share/elasticsearch/bin/elasticsearch start &
 sudo rm -rf /home/ubuntu/saltstack
 
 
